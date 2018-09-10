ae6f5b0b13a389fe525b8fd57b38213b798d1a4b
Naoki Yoshioka <yoshioka.naoki@gmail.com>
2018-09-10 13:43:43 +0900

    Implement topology base class

---
+++ include/yampi/topology.hpp
@@ -0,0 +1,97 @@
+#ifndef YAMPI_TOPOLOGY_HPP
+# define YAMPI_TOPOLOGY_HPP
+
+# include <boost/config.hpp>
+
+# include <utility>
+# ifndef BOOST_NO_CXX11_ADDRESSOF
+#   include <memory>
+# else
+#   include <boost/core/addressof.hpp>
+# endif
+
+# include <mpi.h>
+
+# include <yampi/communicator.hpp>
+# include <yampi/environment.hpp>
+# include <yampi/utility/is_nothrow_swappable.hpp>
+
+# ifndef BOOST_NO_CXX11_ADDRESSOF
+#   define YAMPI_addressof std::addressof
+# else
+#   define YAMPI_addressof boost::addressof
+# endif
+
+
+namespace yampi
+{
+  class topology
+  {
+   protected:
+    ::yampi::communicator communicator_;
+
+   public:
+# ifndef BOOST_NO_CXX11_DELETED_FUNCTIONS
+    topology() = delete;
+    topology(topology const&) = delete;
+    topology& operator=(topology const&) = delete;
+# else // BOOST_NO_CXX11_DELETED_FUNCTIONS
+   private:
+    topology();
+    topology(topology const&);
+    topology& operator=(topology const&);
+
+   public:
+# endif // BOOST_NO_CXX11_DELETED_FUNCTIONS
+# ifndef BOOST_NO_CXX11_RVALUE_REFERENCES
+#   ifndef BOOST_NO_CXX11_DEFAULTED_FUNCTIONS
+    topology(topology&&) = default;
+    topology& operator=(topology&&) = default;
+#   else // BOOST_NO_CXX11_DEFAULTED_FUNCTIONS
+    topology(topology&& other) : communicator_(std::move(other.communicator_)) { }
+    topology& operator=(topology&& other)
+    {
+      if (this != YAMPI_addressof(other))
+        communicator_ = std::move(other.communicator_);
+      return *this;
+    }
+#   endif // BOOST_NO_CXX11_DEFAULTED_FUNCTIONS
+# endif // BOOST_NO_CXX11_RVALUE_REFERENCES
+
+   protected:
+# ifndef BOOST_NO_CXX11_DEFAULTED_FUNCTIONS
+    ~topology() = default;
+# else // BOOST_NO_CXX11_DEFAULTED_FUNCTIONS
+    ~topology() { }
+# endif // BOOST_NO_CXX11_DEFAULTED_FUNCTIONS
+
+   public:
+    explicit topology(MPI_Comm const mpi_comm)
+      : communicator_(mpi_comm)
+    { }
+
+   public:
+    void release(::yampi::environment const& environment)
+    { communicator_.release(environment); }
+
+
+    ::yampi::communicator const& communicator() const { return communicator_; }
+
+    void swap(topology& other)
+      BOOST_NOEXCEPT_IF(( ::yampi::utility::is_nothrow_swappable< ::yampi::communicator >::value ))
+    {
+      using std::swap;
+      swap(communicator_, other.communicator_);
+    }
+  };
+
+  inline void swap(::yampi::topology& lhs, ::yampi::topology& rhs)
+    BOOST_NOEXCEPT(BOOST_NOEXCEPT_EXPR(lhs.swap(rhs)))
+  { lhs.swap(rhs); }
+}
+
+
+# undef YAMPI_addressof
+
+#endif
+
