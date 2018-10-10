dddee69badab3e9cc38a0cd0874278d31450e0de
Naoki Yoshioka <yoshioka.naoki@gmail.com>
2018-10-10 19:15:54 +0900

    Implement one-sided communications; put and get

---
+++ include/yampi/get.hpp
@@ -0,0 +1,38 @@
+#ifndef YAMPI_GET_HPP
+# define YAMPI_GET_HPP
+
+# include <boost/config.hpp>
+
+# include <mpi.h>
+
+# include <yampi/window.hpp>
+# include <yampi/environment.hpp>
+# include <yampi/buffer.hpp>
+# include <yampi/rank.hpp>
+# include <yampi/error.hpp>
+
+
+namespace yampi
+{
+  template <typename Value, typename ContiguousIterator>
+  inline void get(
+    ::yampi::window<Value> const& window, ::yampi::environment const& environment,
+    ::yampi::buffer<Value> const& origin_buffer,
+    ::yampi::rank const target, int const target_index,
+    ::yampi::datatype const& target_datatype = origin_buffer.datatype(),
+    int const target_count = origin_buffer.count())
+  {
+    int const error_code
+      = MPI_Put(
+          origin_buffer.data(), origin_buffer.count(), origin_buffer.datatype().mpi_datatype(),
+          target.mpi_rank(), static_cast<MPI_Aint>(target_index),
+          target_count, target_datatype.mpi_datatype(),
+          window.mpi_win());
+    if (error_code != MPI_SUCCESS)
+      throw ::yampi::error(error_code, "yampi::get", environment);
+  }
+}
+
+
+#endif
+
