5d44562fa2cf065d9b270b77ee67b3393c0b43c3
Naoki Yoshioka <yoshioka.naoki@gmail.com>
2021-10-26 19:00:28 +0900

    Change datatype to use yampi::count and yampi::extent

--- include/yampi/datatype_base.hpp
+++ include/yampi/datatype_base.hpp
@@ -5,7 +5,6 @@
 
 # include <mpi.h>
 
-# include <yampi/bounds.hpp>
 
 namespace yampi
 {
@@ -13,16 +12,6 @@ namespace yampi
   class datatype_base
   {
    public:
-# if MPI_VERSION >= 3
-    typedef MPI_Count size_type;
-    typedef MPI_Count count_type;
-    typedef ::yampi::bounds<count_type> bounds_type;
-# else
-    typedef int size_type;
-    typedef MPI_Aint count_type;
-    typedef ::yampi::bounds<count_type> bounds_type;
-# endif
-
     bool is_null() const BOOST_NOEXCEPT_OR_NOTHROW { return derived().do_is_null(); }
 
     MPI_Datatype mpi_datatype() const BOOST_NOEXCEPT_OR_NOTHROW { return derived().do_mpi_datatype(); }
