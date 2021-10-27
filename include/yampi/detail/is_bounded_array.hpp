d0b3bc96581ab7074a4e26e41b943b753847882c
Naoki Yoshioka <yoshioka.naoki@gmail.com>
2021-10-27 16:48:24 +0900

    Add make_unique and make_shared

---
+++ include/yampi/detail/is_bounded_array.hpp
@@ -0,0 +1,43 @@
+#ifndef YAMPI_DETAIL_IS_BOUNDED_ARRAY_HPP
+# define YAMPI_DETAIL_IS_BOUNDED_ARRAY_HPP
+
+# include <boost/config.hpp>
+
+# include <cstddef>
+# ifndef BOOST_NO_CXX11_HDR_TYPE_TRAITS
+#   include <type_traits>
+# else
+#   include <boost/type_traits/integral_constant.hpp>
+# endif
+
+# ifndef BOOST_NO_CXX11_HDR_TYPE_TRAITS
+#   define YAMPI_true_type std::true_type
+#   define YAMPI_false_type std::false_type
+# else
+#   define YAMPI_true_type boost::true_type
+#   define YAMPI_false_type boost::false_type
+# endif
+
+
+namespace yampi
+{
+  namespace detail
+  {
+    template <typename T>
+    struct is_bounded_array
+      : YAMPI_false_type
+    { };
+
+    template <typename T, std::size_t bound>
+    struct is_bounded_array<T[bound]>
+      : YAMPI_true_type
+    { };
+  }
+}
+
+
+# undef YAMPI_false_type
+# undef YAMPI_true_type
+
+#endif
+
