f144d5787c3401656630460dce9feae6224f95ea
Naoki Yoshioka <yoshioka.naoki@gmail.com>
2022-07-14 18:57:47 +0900

    Change structures of some CRTP classes

--- include/yampi/dynamic_window.hpp
+++ include/yampi/dynamic_window.hpp
@@ -3,9 +3,6 @@
 
 # include <utility>
 # include <type_traits>
-# if __cplusplus < 201703L
-#   include <boost/type_traits/is_nothrow_swappable.hpp>
-# endif
 # include <memory>
 
 # include <mpi.h>
@@ -14,16 +11,7 @@
 # include <yampi/error.hpp>
 # include <yampi/window_base.hpp>
 # include <yampi/communicator.hpp>
-# include <yampi/addressof.hpp>
 # include <yampi/information.hpp>
-# include <yampi/group.hpp>
-# include <yampi/byte_displacement.hpp>
-
-# if __cplusplus >= 201703L
-#   define YAMPI_is_nothrow_swappable std::is_nothrow_swappable
-# else
-#   define YAMPI_is_nothrow_swappable boost::is_nothrow_swappable
-# endif
 
 
 # if MPI_VERSION >= 3
@@ -32,56 +20,26 @@ namespace yampi
   class dynamic_window
     : public ::yampi::window_base< ::yampi::dynamic_window >
   {
-    typedef ::yampi::window_base< ::yampi::dynamic_window > super_type;
+    typedef ::yampi::window_base< ::yampi::dynamic_window > base_type;
 
     MPI_Win mpi_win_;
 
    public:
-    dynamic_window() noexcept(std::is_nothrow_copy_constructible<MPI_Win>::value)
-      : mpi_win_{MPI_WIN_NULL}
-    { }
-
+    dynamic_window() = default;
     dynamic_window(dynamic_window const&) = delete;
     dynamic_window& operator=(dynamic_window const&) = delete;
-
-    dynamic_window(dynamic_window&& other)
-      noexcept(
-        std::is_nothrow_move_constructible<MPI_Win>::value
-        and std::is_nothrow_copy_assignable<MPI_Win>::value)
-      : mpi_win_{std::move(other.mpi_win_)}
-    { other.mpi_win_ = MPI_WIN_NULL; }
-
-    dynamic_window& operator=(dynamic_window&& other)
-      noexcept(
-        std::is_nothrow_move_assignable<MPI_Win>::value
-        and std::is_nothrow_copy_assignable<MPI_Win>::value)
-    {
-      if (this != std::addressof(other))
-      {
-        if (mpi_win_ != MPI_WIN_NULL)
-          MPI_Win_free(std::addressof(mpi_win_));
-        mpi_win_ = std::move(other.mpi_win_);
-        other.mpi_win_ = MPI_WIN_NULL;
-      }
-      return *this;
-    }
-
-    ~dynamic_window() noexcept
-    {
-      if (mpi_win_ == MPI_WIN_NULL)
-        return;
-
-      MPI_Win_free(std::addressof(mpi_win_));
-    }
+    dynamic_window(dynamic_window&&) = default;
+    dynamic_window& operator=(dynamic_window&&) = default;
+    ~dynamic_window() noexcept = default;
 
     dynamic_window(::yampi::communicator const& communicator, ::yampi::environment const& environment)
-      : mpi_win_{create(MPI_INFO_NULL, communicator, environment)}
+      : base_type{create(MPI_INFO_NULL, communicator, environment)}
     { }
 
     dynamic_window(
       ::yampi::information const& information,
       ::yampi::communicator const& communicator, ::yampi::environment const& environment)
-      : mpi_win_{create(information.mpi_info(), communicator, environment)}
+      : base_type{create(information.mpi_info(), communicator, environment)}
     { }
 
    private:
@@ -97,159 +55,15 @@ namespace yampi
         : throw ::yampi::error(error_code, "yampi::dynamic_window::create", environment);
     }
 
-   public:
-    bool operator==(dynamic_window const& other) const noexcept { return mpi_win_ == other.mpi_win_; }
+   private:
+    friend base_type;
 
-    bool do_is_null() const noexcept { return mpi_win_ == MPI_WIN_NULL; }
-
-    MPI_Win const& do_mpi_win() const noexcept { return mpi_win_; }
-
-    template <typename T>
-    T* do_base_ptr(::yampi::environment const& environment) const
-    {
-      T* base_ptr;
-      int flag;
-      int const error_code
-        = MPI_Win_get_attr(mpi_win_, MPI_WIN_BASE, base_ptr, std::addressof(flag));
-      return error_code == MPI_SUCCESS and flag
-        ? base_ptr
-        : throw ::yampi::error(error_code, "yampi::window::do_base_ptr", environment);
-    }
-
-    ::yampi::byte_displacement do_size_bytes(::yampi::environment const& environment) const
-    {
-      MPI_Aint size_bytes;
-      int flag;
-      int const error_code
-        = MPI_Win_get_attr(mpi_win_, MPI_WIN_SIZE, std::addressof(size_bytes), std::addressof(flag));
-      return error_code == MPI_SUCCESS and flag
-        ? ::yampi::byte_displacement(size_bytes)
-        : throw ::yampi::error(error_code, "yampi::window::do_size_bytes", environment);
-    }
-
-    int do_displacement_unit(::yampi::environment const& environment) const
-    {
-      int displacement_unit;
-      int flag;
-      int const error_code
-        = MPI_Win_get_attr(mpi_win_, MPI_WIN_DISP_UNIT, std::addressof(displacement_unit), std::addressof(flag));
-      return error_code == MPI_SUCCESS and flag
-        ? displacement_unit
-        : throw ::yampi::error(error_code, "yampi::window::do_displacement_unit", environment);
-    }
-
-    ::yampi::flavor do_flavor(::yampi::environment const& environment) const
-    {
-      int flavor;
-      int flag;
-      int const error_code
-        = MPI_Win_get_attr(mpi_win_, MPI_WIN_CREATE_FLAVOR, std::addressof(flavor), std::addressof(flag));
-      return error_code == MPI_SUCCESS and flag
-        ? static_cast<::yampi::flavor>(flavor)
-        : throw ::yampi::error(error_code, "yampi::window::do_flavor", environment);
-    }
-
-    ::yampi::memory_model do_memory_model(::yampi::environment const& environment) const
-    {
-      int memory_model;
-      int flag;
-      int const error_code
-        = MPI_Win_get_attr(mpi_win_, MPI_WIN_MODEL, std::addressof(memory_model), std::addressof(flag));
-      return error_code == MPI_SUCCESS and flag
-        ? static_cast<::yampi::memory_model>(memory_model)
-        : throw ::yampi::error(error_code, "yampi::window::do_memory_model", environment);
-    }
-
-    void do_group(::yampi::group& group, ::yampi::environment const& environment) const
-    {
-      MPI_Group mpi_group;
-      int const error_code = MPI_Win_get_group(mpi_win_, std::addressof(mpi_group));
-      if (error_code != MPI_SUCCESS)
-        throw ::yampi::error(error_code, "yampi::window::do_group", environment);
-      group.reset(mpi_group, environment);
-    }
-
-    void free(::yampi::environment const& environment)
-    {
-      if (mpi_win_ == MPI_WIN_NULL)
-        return;
-
-      int const error_code = MPI_Win_free(std::addressof(mpi_win_));
-      if (error_code != MPI_SUCCESS)
-        throw ::yampi::error(error_code, "yampi::window::free", environment);
-    }
-
-    void reset(::yampi::environment const& environment)
-    { free(environment); }
-
-    void reset(MPI_Win const& mpi_win, ::yampi::environment const& environment)
-    {
-      free(environment);
-      mpi_win_ = mpi_win;
-    }
-
-    void reset(dynamic_window&& other, ::yampi::environment const& environment)
-    {
-      free(environment);
-      mpi_win_ = std::move(other.mpi_win_);
-      other.mpi_win_ = MPI_WIN_NULL;
-    }
-
-    template <typename ContiguousIterator>
-    void reset(
-      ContiguousIterator const first, ContiguousIterator const last,
-      ::yampi::communicator const& communicator,
-      ::yampi::environment const& environment)
-    {
-      free(environment);
-      mpi_win_ = create(MPI_INFO_NULL, communicator, environment);
-    }
-
-    template <typename ContiguousIterator>
-    void reset(
-      ContiguousIterator const first, ContiguousIterator const last,
-      ::yampi::information const& information,
-      ::yampi::communicator const& communicator,
-      ::yampi::environment const& environment)
-    {
-      free(environment);
-      mpi_win_ = create(information.mpi_info(), communicator, environment);
-    }
-
-    void set_information(yampi::information const& information, yampi::environment const& environment) const
-    {
-      int const error_code = MPI_Win_set_info(mpi_win_, information.mpi_info());
-      if (error_code != MPI_SUCCESS)
-        throw ::yampi::error(error_code, "yampi::dynamic_window::set_information", environment);
-    }
-
-    void get_information(yampi::information& information, yampi::environment const& environment) const
-    {
-      MPI_Info result;
-      int const error_code = MPI_Win_get_info(mpi_win_, std::addressof(result));
-
-      if (error_code != MPI_SUCCESS)
-        throw ::yampi::error(error_code, "yampi::dynamic_window::get_information", environment);
-      information.reset(result, environment);
-    }
-
-    void swap(dynamic_window& other) noexcept(YAMPI_is_nothrow_swappable<MPI_Win>::value)
-    {
-      using std::swap;
-      swap(mpi_win_, other.mpi_win_);
-    }
+    void do_reset(dynamic_window&& other, ::yampi::environment const& environment) { }
+    void do_swap(window& other) noexcept { }
   };
-
-  inline bool operator!=(::yampi::dynamic_window const& lhs, ::yampi::dynamic_window const& rhs) noexcept(noexcept(lhs == rhs))
-  { return not (lhs == rhs); }
-
-  inline void swap(::yampi::dynamic_window& lhs, ::yampi::dynamic_window& rhs) noexcept(noexcept(lhs.swap(rhs)))
-  { lhs.swap(rhs); }
 }
 # endif // MPI_VERSION >= 3
 
-
-# undef YAMPI_is_nothrow_swappable
 
 #endif
 
--- include/yampi/window_base.hpp
+++ include/yampi/window_base.hpp
@@ -1,11 +1,25 @@
 #ifndef YAMPI_WINDOW_BASE_HPP
 # define YAMPI_WINDOW_BASE_HPP
 
+# include <type_traits>
+# if __cplusplus < 201703L
+#   include <boost/type_traits/is_nothrow_swappable.hpp>
+# endif
+# include <utility>
+# include <memory>
+
 # include <mpi.h>
 
 # include <yampi/environment.hpp>
 # include <yampi/byte_displacement.hpp>
 # include <yampi/group.hpp>
+# include <yampi/error.hpp>
+
+# if __cplusplus >= 201703L
+#   define YAMPI_is_nothrow_swappable std::is_nothrow_swappable
+# else
+#   define YAMPI_is_nothrow_swappable boost::is_nothrow_swappable
+# endif
 
 
 namespace yampi
@@ -26,28 +40,194 @@ namespace yampi
   template <typename Derived>
   class window_base
   {
-   public:
-    bool is_null() const noexcept { return derived().do_is_null(); }
+   protected:
+    MPI_Win mpi_win_;
 
-    MPI_Win const& mpi_win() const noexcept { return derived().do_mpi_win(); }
+   public:
+    window_base() noexcept(std::is_nothrow_copy_constructible<MPI_Win>::value)
+      : mpi_win_{MPI_WIN_NULL}
+    { }
+
+    window_base(window_base const&) = delete;
+    window_base& operator=(window_base const&) = delete;
+
+    window_base(window_base&& other)
+      noexcept(
+        std::is_nothrow_move_constructible<MPI_Win>::value
+        and std::is_nothrow_copy_assignable<MPI_Win>::value)
+      : mpi_win_{std::move(other.mpi_win_)}
+    { other.mpi_win_ = MPI_WIN_NULL; }
+
+    window_base& operator=(window_base&& other)
+      noexcept(
+        std::is_nothrow_move_assignable<MPI_Win>::value
+        and std::is_nothrow_copy_assignable<MPI_Win>::value)
+    {
+      if (this != std::addressof(other))
+      {
+        if (mpi_win_ != MPI_WIN_NULL)
+          MPI_Win_free(std::addressof(mpi_win_));
+        mpi_win_ = std::move(other.mpi_win_);
+        other.mpi_win_ = MPI_WIN_NULL;
+      }
+      return *this;
+    }
+
+   protected:
+    ~window_base() noexcept
+    {
+      if (mpi_win_ == MPI_WIN_NULL)
+        return;
+
+      MPI_Win_free(std::addressof(mpi_win_));
+    }
+
+   public:
+    explicit window_base(MPI_Win const& mpi_win)
+      noexcept(std::is_nothrow_copy_constructible<MPI_Win>::value)
+      : mpi_win_{mpi_win}
+    { }
+
+    void free(::yampi::environment const& environment)
+    {
+      if (mpi_win_ == MPI_WIN_NULL)
+        return;
+
+      int const error_code = MPI_Win_free(std::addressof(mpi_win_));
+      if (error_code != MPI_SUCCESS)
+        throw ::yampi::error{error_code, "yampi::window_base::free", environment};
+    }
+
+    void reset(::yampi::environment const& environment)
+    { free(environment); }
+
+    void reset(MPI_Win const& mpi_win, ::yampi::environment const& environment)
+    {
+      free(environment);
+      mpi_win_ = mpi_win;
+    }
+
+    void reset(Derived&& other, ::yampi::environment const& environment)
+    {
+      free(environment);
+      mpi_win_ = std::move(other.mpi_win_);
+      other.mpi_win_ = MPI_WIN_NULL;
+      derived().do_reset(std::move(other), environment);
+    }
+
+    bool is_null() const noexcept(noexcept(mpi_win_ == MPI_WIN_NULL)) { return mpi_win_ == MPI_WIN_NULL; }
+
+    bool operator==(window_base const& other) const noexcept { return mpi_win_ == other.mpi_win_; }
+
+    void group(::yampi::group& result, ::yampi::environment const& environment) const
+    {
+      MPI_Group mpi_group;
+      int const error_code = MPI_Win_get_group(mpi_win_, std::addressof(mpi_group));
+      if (error_code != MPI_SUCCESS)
+        throw ::yampi::error{error_code, "yampi::window_base::group", environment};
+      result.reset(mpi_group, environment);
+    }
 
     template <typename T>
-    T* base_ptr(::yampi::environment const& environment) const { return derived().template do_base_ptr<T>(environment); }
-    ::yampi::byte_displacement size_bytes(::yampi::environment const& environment) const { return derived().do_size_bytes(environment); }
-    int displacement_unit(::yampi::environment const& environment) const { return derived().do_displacement_unit(environment); }
-# if MPI_VERSION >= 3
-    ::yampi::flavor flavor(::yampi::environment const& environment) const { return derived().do_flavor(environment); }
-    ::yampi::memory_model memory_model(::yampi::environment const& environment) const { return derived().do_memory_model(environment); }
-# endif // MPI_VERSION >= 3
+    T* base_ptr(::yampi::environment const& environment) const
+    {
+      T* result;
+      int flag;
+      int const error_code
+        = MPI_Win_get_attr(mpi_win_, MPI_WIN_BASE, result, std::addressof(flag));
+      return error_code == MPI_SUCCESS and flag
+        ? result
+        : throw ::yampi::error{error_code, "yampi::window_base::base_ptr", environment};
+    }
 
-    void group(::yampi::group& group, ::yampi::environment const& environment) const { derived().do_group(group, environment); }
+    ::yampi::byte_displacement size_bytes(::yampi::environment const& environment) const
+    {
+      MPI_Aint result;
+      int flag;
+      int const error_code
+        = MPI_Win_get_attr(mpi_win_, MPI_WIN_SIZE, std::addressof(result), std::addressof(flag));
+      return error_code == MPI_SUCCESS and flag
+        ? ::yampi::byte_displacement(result)
+        : throw ::yampi::error{error_code, "yampi::window_base::size_bytes", environment};
+    }
+
+    int displacement_unit(::yampi::environment const& environment) const
+    {
+      int result;
+      int flag;
+      int const error_code
+        = MPI_Win_get_attr(mpi_win_, MPI_WIN_DISP_UNIT, std::addressof(result), std::addressof(flag));
+      return error_code == MPI_SUCCESS and flag
+        ? result
+        : throw ::yampi::error{error_code, "yampi::window_base::displacement_unit", environment};
+    }
+
+# if MPI_VERSION >= 3
+    ::yampi::flavor flavor(::yampi::environment const& environment) const
+    {
+      int result;
+      int flag;
+      int const error_code
+        = MPI_Win_get_attr(mpi_win_, MPI_WIN_CREATE_FLAVOR, std::addressof(result), std::addressof(flag));
+      return error_code == MPI_SUCCESS and flag
+        ? static_cast<::yampi::flavor>(result)
+        : throw ::yampi::error{error_code, "yampi::window_base::flavor", environment};
+    }
+
+    ::yampi::memory_model memory_model(::yampi::environment const& environment) const
+    {
+      int result;
+      int flag;
+      int const error_code
+        = MPI_Win_get_attr(mpi_win_, MPI_WIN_MODEL, std::addressof(result), std::addressof(flag));
+      return error_code == MPI_SUCCESS and flag
+        ? static_cast<::yampi::memory_model>(result)
+        : throw ::yampi::error{error_code, "yampi::window_base::memory_model", environment};
+    }
+
+    void set_information(yampi::information const& information, yampi::environment const& environment) const
+    {
+      int const error_code = MPI_Win_set_info(mpi_win_, information.mpi_info());
+      if (error_code != MPI_SUCCESS)
+        throw ::yampi::error{error_code, "yampi::window_base::set_information", environment};
+    }
+
+    void get_information(yampi::information& information, yampi::environment const& environment) const
+    {
+      MPI_Info result;
+      int const error_code = MPI_Win_get_info(mpi_win_, std::addressof(result));
+
+      if (error_code != MPI_SUCCESS)
+        throw ::yampi::error{error_code, "yampi::window_base::get_information", environment};
+      information.reset(result, environment);
+    }
+# endif
+
+    MPI_Win const& mpi_win() const noexcept { return mpi_win_; }
+
+    void swap(Derived& other) noexcept(YAMPI_is_nothrow_swappable<MPI_Win>::value)
+    {
+      using std::swap;
+      swap(mpi_win_, other.mpi_win_);
+      derived().do_swap(other);
+    }
 
    protected:
     Derived& derived() noexcept { return static_cast<Derived&>(*this); }
     Derived const& derived() const noexcept { return static_cast<Derived const&>(*this); }
   };
+
+  template <typename Derived>
+  inline bool operator!=(::yampi::window_base<Derived> const& lhs, ::yampi::window_base<Derived> const& rhs) noexcept(noexcept(lhs == rhs))
+  { return not (lhs == rhs); }
+
+  template <typename Derived>
+  inline void swap(::yampi::window_base<Derived>& lhs, ::yampi::window_base<Derived>& rhs) noexcept(noexcept(lhs.swap(rhs)))
+  { lhs.swap(rhs); }
 }
 
+
+# undef YAMPI_is_nothrow_swappable
 
 #endif
 
